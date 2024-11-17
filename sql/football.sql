c7e7d10cb93a766fab1526ac812732a614df9d1e
khp9798 <121469544+khp9798@users.noreply.github.com>
2024-11-17 23:08:41 +0900

    Update football.sql

--- sql/football.sql
+++ sql/football.sql
@@ -1,7 +1,6 @@
 CREATE DATABASE if not exists flapfootball;
 USE flapfootball;
 
-drop table users;
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,           -- 시스템 내부 고유 식별자
     userid VARCHAR(50) NOT NULL UNIQUE,        -- 로그인용 아이디
