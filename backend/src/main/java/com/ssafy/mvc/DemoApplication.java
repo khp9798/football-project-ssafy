928058a7494ba62939616e89725560b4a0c06c57
khp9798 <jmn9798@naver.com>
2024-11-18 16:32:27 +0900

    update

--- backend/src/main/java/com/ssafy/mvc/DemoApplication.java
+++ backend/src/main/java/com/ssafy/mvc/DemoApplication.java
@@ -3,7 +3,7 @@ package com.ssafy.mvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.ssafy.mvc")
 public class DemoApplication {
 
 	public static void main(String[] args) {
