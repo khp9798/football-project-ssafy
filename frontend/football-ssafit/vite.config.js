24a543ca445c47f856101c696b9a332aaaae078d
khp9798 <121469544+khp9798@users.noreply.github.com>
2024-11-21 21:21:31 +0900

    Update vite.config.js

--- frontend/football-ssafit/vite.config.js
+++ frontend/football-ssafit/vite.config.js
@@ -6,14 +6,15 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/football-project-ssafy/",
+  base: '/football-project-ssafy/', // 앞뒤에 슬래시를 추가해야 함
   plugins: [
     vue(),
     vueDevTools(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
+
